5d85586b73809a5c034ed081ba74a18ad4e718a6
Christos <christos342@gmail.com>
2018-02-14 04:09:14 +0200

    Initial Release

---
+++ kMP3Dumper/Form1.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace kMP3Dumper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.savePath = new System.Windows.Forms.FolderBrowserDialog();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Log = new System.Windows.Forms.RichTextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.LoadTags = new System.Windows.Forms.OpenFileDialog();
+            this.AlbumArt = new System.Windows.Forms.PictureBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.AlbumArt)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(68, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(296, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Media Link:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(68, 45);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(253, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Save to:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(327, 43);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(38, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 72);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(358, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Download";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Log);
+            this.groupBox1.Location = new System.Drawing.Point(12, 119);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(370, 202);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Log";
+            // 
+            // Log
+            // 
+            this.Log.Location = new System.Drawing.Point(6, 19);
+            this.Log.Name = "Log";
+            this.Log.ReadOnly = true;
+            this.Log.Size = new System.Drawing.Size(358, 177);
+            this.Log.TabIndex = 0;
+            this.Log.Text = "";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(370, 101);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Media";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.textBox5);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.AlbumArt);
+            this.groupBox3.Controls.Add(this.button4);
+            this.groupBox3.Controls.Add(this.button3);
+            this.groupBox3.Controls.Add(this.textBox3);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.textBox4);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Location = new System.Drawing.Point(388, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(370, 309);
+            this.groupBox3.TabIndex = 8;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Tags";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(45, 19);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(320, 20);
+            this.textBox3.TabIndex = 0;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Title:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(45, 42);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(320, 20);
+            this.textBox4.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 45);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Artist:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(6, 280);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(358, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Apply";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(6, 251);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(358, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Load";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // LoadTags
+            // 
+            this.LoadTags.FileName = "File.mp3";
+            // 
+            // AlbumArt
+            // 
+            this.AlbumArt.Location = new System.Drawing.Point(6, 87);
+            this.AlbumArt.Name = "AlbumArt";
+            this.AlbumArt.Size = new System.Drawing.Size(358, 158);
+            this.AlbumArt.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.AlbumArt.TabIndex = 8;
+            this.AlbumArt.TabStop = false;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(45, 65);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(320, 20);
+            this.textBox5.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(7, 68);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Album:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(766, 327);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(782, 366);
+            this.MinimumSize = new System.Drawing.Size(782, 366);
+            this.Name = "Form1";
+            this.Text = "kPanel - MP3 Dumper";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.AlbumArt)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.FolderBrowserDialog savePath;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox Log;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.OpenFileDialog LoadTags;
+        private System.Windows.Forms.PictureBox AlbumArt;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ kMP3Dumper/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kMP3Dumper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            if (!File.Exists(AppDomain.CurrentDomain.BaseDirectory + "\\ffmpeg.exe")) { File.WriteAllBytes(AppDomain.CurrentDomain.BaseDirectory + "\\ffmpeg.exe", Properties.Resources.ffmpeg); }
+            if (!File.Exists(AppDomain.CurrentDomain.BaseDirectory + "\\ffplay.exe")) { File.WriteAllBytes(AppDomain.CurrentDomain.BaseDirectory + "\\ffplay.exe", Properties.Resources.ffplay); }
+            if (!File.Exists(AppDomain.CurrentDomain.BaseDirectory + "\\ffprobe.exe")) { File.WriteAllBytes(AppDomain.CurrentDomain.BaseDirectory + "\\ffprobe.exe", Properties.Resources.ffprobe); }
+            if (!File.Exists(AppDomain.CurrentDomain.BaseDirectory + "\\youtube-dl.exe")) { File.WriteAllBytes(AppDomain.CurrentDomain.BaseDirectory + "\\youtube-dl.exe", Properties.Resources.youtube_dl); }
+            if (!File.Exists(AppDomain.CurrentDomain.BaseDirectory + "\\taglib-sharp.dll")) { File.WriteAllBytes(AppDomain.CurrentDomain.BaseDirectory + "\\taglib-sharp.dll", Properties.Resources.taglib_sharp); }
+            if (!File.Exists(AppDomain.CurrentDomain.BaseDirectory + "\\policy.2.0.taglib-sharp.dll")) { File.WriteAllBytes(AppDomain.CurrentDomain.BaseDirectory + "\\policy.2.0.taglib-sharp.dll", Properties.Resources.policy_2_0_taglib_sharp); }
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if ((textBox1.Text.Length > 3) && (textBox2.Text.Length > 3))
+            {
+                new Thread((ThreadStart)(() =>
+                {
+                    var proc = new Process
+                    {
+                        StartInfo = new ProcessStartInfo
+                        {
+                            FileName = "youtube-dl.exe",
+                            Arguments = Properties.Resources.ytdl_args + " " + textBox1.Text,
+                            UseShellExecute = false,
+                            RedirectStandardOutput = true,
+                            CreateNoWindow = true
+                        }
+                    };
+                    Log.Clear();
+                    button2.Text = "Downloading...";
+                    button2.Enabled = false;
+                    button1.Enabled = false;
+                    textBox1.ReadOnly = true;
+                    textBox2.ReadOnly = true;
+                    proc.Start();
+                    while (!proc.StandardOutput.EndOfStream)
+                    {
+                        Log.AppendText(proc.StandardOutput.ReadLine() + Environment.NewLine);
+                    }
+                    string[] files = System.IO.Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory, "*.mp3");
+                    foreach (string dlfile in files)
+                    {
+                        File.Copy(dlfile, textBox2.Text + "\\" + dlfile.Replace(AppDomain.CurrentDomain.BaseDirectory, ""));
+                        File.Delete(dlfile);
+                    }
+                    button2.Text = "Download";
+                    button2.Enabled = true;
+                    button1.Enabled = true;
+                    textBox1.ReadOnly = false;
+                    textBox2.ReadOnly = false;
+                    MessageBox.Show("Done!", "kPanel - MP3 Dumper", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                })).Start();
+            } else
+            {
+                MessageBox.Show("Please provide a valid URL and Save Path!", "kPanel - MP3 Dumper", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            savePath.ShowDialog();
+            textBox2.Text = savePath.SelectedPath;
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (File.Exists(LoadTags.FileName))
+                {
+                    TagLib.File file = TagLib.File.Create(LoadTags.FileName);
+                    try { file.Tag.Title = textBox3.Text; } catch { }
+                    try { file.Tag.Performers = null; file.Tag.Performers = new[] { textBox4.Text }; } catch { }
+                    try { file.Tag.Album = textBox5.Text; } catch { }
+                    try { file.Save(); } catch { }
+                    MessageBox.Show("Tags applied!", "kPanel - MP3 Dumper", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Something went wrong." + Environment.NewLine + "--------------------------------------" + Environment.NewLine + ex, "kPanel - MP3 Dumper", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            LoadTags.ShowDialog();
+            AlbumArt.Image = null;
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+            try
+            {
+                if (File.Exists(LoadTags.FileName))
+                {
+                    TagLib.File file = TagLib.File.Create(LoadTags.FileName);
+                    try { textBox3.Text = file.Tag.Title; } catch { }
+                    try { textBox4.Text = file.Tag.Performers[0]; } catch { }
+                    try { textBox5.Text = file.Tag.Album; } catch { }
+                    try { MemoryStream ms = new MemoryStream(file.Tag.Pictures[0].Data.Data); AlbumArt.Image = System.Drawing.Image.FromStream(ms); } catch { }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Something went wrong." + Environment.NewLine + "--------------------------------------" + Environment.NewLine + ex, "kPanel - MP3 Dumper", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
